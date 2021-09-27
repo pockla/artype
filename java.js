647c68fef830bfa13133930ddabdb5f947a46e44
pockla <ho9805328751623@gamil.com>
2021-09-27 23:41:43 +0800

    sidevar modigy

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 // Script to open and close sidebar
-function w3_open() {
+  function bar_open() {
     document.getElementById("mySidebar").style.display = "block";
   }
    
-  function w3_close() {
+  function bar_close() {
     document.getElementById("mySidebar").style.display = "none";
   }
